387ccb39c679d13aac1149d7d01c9093efcc8137
Paulo Menezes <paulo.hgmenezes@gmail.com>
2022-09-03 22:30:56 -0300

    Initial Commit

---
+++ Finanzas/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Finanzas
+//
+//  Created by Paulo Menezes on 03/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Finanzas/FinanzasApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanzasApp.swift
+//  Finanzas
+//
+//  Created by Paulo Menezes on 03/09/22.
+//
+
+import SwiftUI
+
+@main
+struct FinanzasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
